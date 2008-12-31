00e665b29a66db09c742a255e0937888136bab48
kk <2582892258@qq.com>
2009-01-01 01:25:21 +0800

    update

--- test.py
+++ test.py
@@ -1,23 +1,45 @@
+#def test(info):
+    #if info.username=="root"and info.passwd=="1223":
+        #print("你有权限")
+    #else:
+        #print("你没有权限")
+        #return
+    #return"123"
+#def test2(info):
+    #if info.username=="root"and info.passwd=="1223":
+      # print("你有权限")
+    #else:
+       #print("你没有权限")
+     #  return
+   # return"456"
+#@permit
+#def test2(info):
+    #return"456"
+
+#permit
+#def test(info):
+    #return"123"
+------------------------------------------
 def permit():
-    def test(info):
-        if info.username=="root"and info.passwd=="1223":
+    def log(username,passwd):
+        if username=="root"and passwd=="1223":
             print("你有权限")
         else:
             print("你没有权限")
             return
         return"123"
-def test2(info):
-    if info.username=="root"and info.passwd=="1223":
-       print("你有权限")
-    else:
-       print("你没有权限")
-       return
-    return"456"
 
-#@permit
-def test2(info):
-    return"456"
+@permit()
+def log(username,passwd):
+     return "123"
 
-#@permit
-def test(info):
-    return"123"
+log("root",1223)
+
+
+
+
+
+
+
+
+
