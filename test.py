f99eb9f44549241e3215e892bf5285a6161176f2
kk <2582892258@qq.com>
2009-01-01 00:32:00 +0800

    update

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("hello guys")
+def test(info):
+    if info.username=="root"and info.passwd=="1223":
+        print("你有权限")
+    else:
+        print("你没有权限")
+        return
+    return"123"
+
+def test2(info):
+    if info.username=="root"and info.passwd=="1223":
+        print("你有权限")
+    else:
+        print("你没有权限")
+        return
+    return"456"
+
+@permit
+def test2(info)
+    return"456"
+
+@permit
+def test(info)
+    return"123"
